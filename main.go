85de79b757c3edef5b88b6705086a20b33b80c5b
Mick Luypaerts <mickluypaerts@gmail.com>
2021-07-08 17:56:04 +0200

    first test handler hello and server setup.

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"brewery/api/handlers"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
 
+func main() {
+	l := log.New(os.Stdout, "brewery-api ", log.LstdFlags)
+	helloHandler := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", helloHandler)
+	log.Printf("Starting  the server on 8080\n")
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: sm,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt) // Notify broadcast a message on the sigChan when an Interrupt is received
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown.", sig)
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(tc)
 }
